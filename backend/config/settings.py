07b9b68c64a62c12f2d60bf387b4dfa237469306
Azamat <bakytbekovazamat45@gmail.com>
2022-06-03 23:35:41 +0600

    add postgres database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "site_news",
+        'USER': "news_user",
+        'PASSWORD': "qwerty123",
+        'HOST': "127.0.0.1",
+        'PORT': "5432"
     }
 }
 
